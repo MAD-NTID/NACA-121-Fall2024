327566fa69ff59af16a8df94eafc67b1521ebfa6
Kemoy Campbell <kscics@rit.edu>
2024-08-26 15:24:09 -0400

    MyFirstProgram. A demo on how to translate python code to C#

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿string option = "3";
+
+if(option == "1" || option == "2")
+    Console.WriteLine("You chose option " + option);
+else if(option == "3")
+    Console.WriteLine($"choice {option} is magnificient!");
+else
+    Console.WriteLine("You did not choose a valid option");
+
+//Understanding WriteLine
+Console.WriteLine("Hello NACA"); //Console.Write("Hello NACA\n");
