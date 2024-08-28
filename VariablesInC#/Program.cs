047a0c6193ff01682d55d833b50732df13ac5698
Kemoy Campbell <kscics@rit.edu>
2024-08-28 15:18:22 -0400

    data types

---
+++ VariablesInC#/Program.cs
@@ -0,0 +1,31 @@
+﻿int age = 20;
+string name = "Travis";
+string major = "Game Designing";
+
+
+
+
+Console.WriteLine("Name:" + name + " Age:"+ age + " Major:"+major);
+
+Console.WriteLine($"Name:{name} Age:{age} Major:{major}");
+
+Console.WriteLine("Name:{0} Age:{1} Major:{2}", name, age, major);
+
+//Type conversions
+
+int foo  = 5;
+long foo2 = 20;
+double meh = 9.5;
+foo2 = foo; //implicit type conversion
+
+foo = (int)foo2; //explicit conversion
+
+foo = (int)meh;////explicit conversion
+
+string fooInStringNumeric = "20";
+//there are .Parse for all of the different bultin types
+int fooStringNumericToActualNumber = int.Parse(fooInStringNumeric); //approach one using .parse
+
+
+//There are Convert.<something> for the other types as well
+int fooStringNumbericToActualUsingConvert  = Convert.ToInt32(fooInStringNumeric);
