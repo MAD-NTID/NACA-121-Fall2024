6c559010b08d301624c47356e9bf10d1a8c8cad4
Kemoy Campbell <kscics@rit.edu>
2024-09-06 15:24:15 -0400

    method and array

---
+++ Array/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] numbers = new int[5];
+
+//assign 7 at index 0
+numbers[0] = 7;
+
+Console.WriteLine(numbers[0]);
+
+//printing the values using a loop
+Console.WriteLine();
+for(int i = 0; i < numbers.Length; i++)
+    Console.WriteLine(numbers[i]);
+
+Console.WriteLine();
+foreach(int num in numbers) //for num in numbers
+    Console.WriteLine(num);
+
+string name ="Kemoy Campbell";
+foreach(char letter in name)
+    Console.WriteLine(letter);
+
+
+//user determines the size of the array
+int[] number2;
+
+Console.Write("How many numbers:");
+int size = int.Parse(Console.ReadLine());
+
+number2 = new int[size];
+
+//assigning values to an array without specifying the size
+int[] number3 = {1,2,3,4,5,6,7};
+//number3[0] = 1
+//number3[1] = 2
+//number3[2] = 3
---
+++ Method/Program.cs
@@ -0,0 +1,49 @@
+﻿// int Add(int num1, int num2) //def add(num1, num2)
+// {
+//     int sum = num1 + num2;
+//     return sum;
+// }
+
+int Add(int num1, int num2) //def add(num1, num2)
+{
+    return num1 + num2;
+}
+
+int result = Add(7,6);
+Console.WriteLine(result);
+
+Console.WriteLine("The result is:" + Add(7,6));
+
+void GreetHuman(string human)
+{
+    Console.WriteLine("Hello human:"+ human);
+    return ;//optional
+}
+
+GreetHuman("Kemoy");
+
+int Subtract(int num1, int num2 = 5)
+{
+    return num1- num2;
+}
+
+Console.WriteLine(Subtract(5,10));
+
+Console.WriteLine(Subtract(5));
+
+bool Convert(string numeric, out int number)
+{
+    number = int.Parse(numeric);
+    return true;
+}
+
+/*
+def convert(numeric):
+    return true, int(numeric)
+*/
+int result2;
+bool success = Convert("2", out result2);
+if(success)
+    Console.WriteLine(result2);
+
+
